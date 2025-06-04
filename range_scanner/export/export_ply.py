3aecee1dc0d9eb0dd8caf1d82aafe317edc93252
jmmuller <jean-michael.muller@ign.fr>
2025-06-04 17:49:35 +0200

    export: add intensity to ply

--- range_scanner/export/export_ply.py
+++ range_scanner/export/export_ply.py
@@ -11,6 +11,8 @@ def export(filePath, fileName, data, exportNoiseData):
     # the method Vector3dVector() will convert numpy array of shape (n, 3) to Open3D format.
     # see http://www.open3d.org/docs/release/python_api/open3d.utility.Vector3dVector.html#open3d.utility.Vector3dVector
     pcd.points = o3d.utility.Vector3dVector(hits_xyz)
+    hits_vvv = np.tile(data[:, 6], (3,1)).T
+    pcd.colors = o3d.utility.Vector3dVector(hits_vvv)
     o3d.io.write_point_cloud(os.path.join(filePath, "%s.ply" % fileName), pcd, write_ascii=False)
     
     if exportNoiseData:
@@ -19,6 +21,7 @@ def export(filePath, fileName, data, exportNoiseData):
         # the method Vector3dVector() will convert numpy array of shape (n, 3) to Open3D format.
         # see http://www.open3d.org/docs/release/python_api/open3d.utility.Vector3dVector.html#open3d.utility.Vector3dVector
         pcd.points = o3d.utility.Vector3dVector(hits_xyz)
+        pcd.colors = o3d.utility.Vector3dVector(hits_vvv)
         o3d.io.write_point_cloud(os.path.join(filePath, "%s_noise.ply" % fileName), pcd, write_ascii=False)
 
     print("Done.")
